e04aae91bff514cffceeb8c31731fa6dd4f2cb40
김현교 <zkdlajt96@naver.com>
2022-09-18 15:17:05 +0900

    :sparkles: feat : 담당 Entity 컬럼 추가
    - 담당한 Entity Class의 컬럼 생성.
    - 단방향 연관관계 매핑.
    - Comment는 부모 자식 양방향 매핑.
    - BaseTimeEntity 상속.

--- BE/src/main/java/com/team7/chaekin/domain/meeting/entity/Meeting.java
+++ BE/src/main/java/com/team7/chaekin/domain/meeting/entity/Meeting.java
@@ -1,4 +1,29 @@
 package com.team7.chaekin.domain.meeting.entity;
 
-public class Meeting {
+import com.team7.chaekin.domain.book.entity.Book;
+import com.team7.chaekin.domain.common.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Meeting extends BaseTimeEntity {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    private String title;
+
+    private String description;
+
+    private int capacity;
+
+    private boolean isRemoved;
 }
--- BE/src/main/java/com/team7/chaekin/domain/reportcomment/entity/ReportComment.java
+++ BE/src/main/java/com/team7/chaekin/domain/reportcomment/entity/ReportComment.java
@@ -1,4 +1,39 @@
 package com.team7.chaekin.domain.reportcomment.entity;
 
-public class ReportComment {
+import com.team7.chaekin.domain.common.entity.BaseTimeEntity;
+import com.team7.chaekin.domain.member.entity.Member;
+import com.team7.chaekin.domain.report.entity.Report;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class ReportComment extends BaseTimeEntity {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "report_id")
+    private Report report;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private ReportComment parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<ReportComment> children = new ArrayList<>();
+
+    private String contents;
+
+    private boolean isRemoved;
 }
