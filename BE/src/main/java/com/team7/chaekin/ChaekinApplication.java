16cb3f3d7c652ec723a45c4f4f799ac72019e1c7
김현교 <zkdlajt96@naver.com>
2022-09-20 21:05:23 +0900

    :recycle: refactor : CommandLineRunner 삭제
    - 테스트를 위해 추가했던 CommandLineRunner 삭제.

--- BE/src/main/java/com/team7/chaekin/ChaekinApplication.java
+++ BE/src/main/java/com/team7/chaekin/ChaekinApplication.java
@@ -1,12 +1,7 @@
 package com.team7.chaekin;
 
-import com.team7.chaekin.domain.book.entity.Book;
-import com.team7.chaekin.domain.category.entity.Category;
-import com.team7.chaekin.domain.category.repository.CategoryRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -15,18 +10,6 @@ public class ChaekinApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChaekinApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner run(CategoryRepository categoryRepository) {
-		return args -> {
-			categoryRepository.save(Category.builder()
-							.cid("asd")
-							.mall("asd")
-							.depth1("asd")
-							.name("asd").build());
-
-		};
 	}
 
 }
