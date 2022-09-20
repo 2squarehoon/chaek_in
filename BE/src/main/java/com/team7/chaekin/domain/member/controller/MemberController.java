5f146f92d92238067b6f2ec58a832bd323db69ce
김현교 <zkdlajt96@naver.com>
2022-09-20 15:41:54 +0900

    :sparkles: feat : Member API
    - Member Domain의 API 기본 코드 작성.
    - 예외처리, 유효성 검사 등 이후에 추가해야 함.
    - BooklogRepository와 연결된 부분도 추가해야 함.

--- BE/src/main/java/com/team7/chaekin/domain/member/controller/MemberController.java
+++ BE/src/main/java/com/team7/chaekin/domain/member/controller/MemberController.java
@@ -1,13 +1,22 @@
 package com.team7.chaekin.domain.member.controller;
 
 import com.team7.chaekin.domain.member.dto.MemberBooksResponse;
-import com.team7.chaekin.domain.member.dto.MemberInfoRequest;
+import com.team7.chaekin.domain.member.dto.MemberCreateRequest;
+import com.team7.chaekin.domain.member.dto.MemberUpdateRequest;
+import com.team7.chaekin.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/members")
 @RestController
 public class MemberController {
+
+    private final MemberService memberService;
+
+    //TODO: 로그인 로직 도입 후 삭제
+    private static long memberId = 1;
 
     @GetMapping("/me/logout")
     public ResponseEntity<?> logout() {
@@ -16,21 +25,26 @@ public class MemberController {
 
     @GetMapping("/{memberId}/books")
     public ResponseEntity<?> getMemberBooks(@PathVariable long memberId) {
-        return ResponseEntity.ok(new MemberBooksResponse());
+        MemberBooksResponse memberBooksResponse = memberService.getMemberBooks(memberId);
+        return ResponseEntity.ok(memberBooksResponse);
     }
 
     @PostMapping("/me")
-    public ResponseEntity<?> saveAdditionalInformation(@RequestBody MemberInfoRequest memberInfoRequest) {
+    public ResponseEntity<?> saveAdditionalInformation(@RequestBody MemberCreateRequest memberCreateRequest) {
+        memberService.saveAdditionalInformation(memberCreateRequest);
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/me")
-    public ResponseEntity<?> updateAdditionalInformation(@RequestBody MemberInfoRequest memberInfoRequest) {
+    public ResponseEntity<?> updateAdditionalInformation(@RequestBody MemberUpdateRequest memberUpdateRequest) {
+        memberService.updateAdditionalInformation(memberId, memberUpdateRequest);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/me")
     public ResponseEntity<?> deleteMember() {
+        memberService.deleteMember(memberId);
+
         return ResponseEntity.noContent().build();
     }
 }
