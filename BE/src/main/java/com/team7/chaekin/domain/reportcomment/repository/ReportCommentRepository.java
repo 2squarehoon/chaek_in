5c27395c0e3690ac76d8316e1b6b644f1c1b2efc
김희영 <ahwl8240@naver.com>
2022-09-20 13:35:14 +0900

    🐛 fix : ReportCommentRepository 타입 수정 ReportCommentRepository -> ReportComment.

--- BE/src/main/java/com/team7/chaekin/domain/reportcomment/repository/ReportCommentRepository.java
+++ BE/src/main/java/com/team7/chaekin/domain/reportcomment/repository/ReportCommentRepository.java
@@ -1,6 +1,7 @@
 package com.team7.chaekin.domain.reportcomment.repository;
 
+import com.team7.chaekin.domain.reportcomment.entity.ReportComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReportCommentRepository extends JpaRepository<ReportCommentRepository, Long> {
+public interface ReportCommentRepository extends JpaRepository<ReportComment, Long> {
 }
