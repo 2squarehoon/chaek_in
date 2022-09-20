f9ec485c9c2ee7a7c1bd6cdb0cb23af9769eac40
InTae <kit938639@gmail.com>
2022-09-20 14:03:33 +0900

    :sparkles: feat : 책 검색 #S07P22A107-108 - 책 검색 기능 구현 완료 - Postman 테스트 완료

--- BE/src/main/java/com/team7/chaekin/domain/book/repository/BookRepository.java
+++ BE/src/main/java/com/team7/chaekin/domain/book/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.team7.chaekin.domain.book.repository;
 
 import com.team7.chaekin.domain.book.entity.Book;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+    Slice<Book> findByTitleContaining(String title, Pageable pageable);
 }
--- BE/src/main/java/com/team7/chaekin/domain/book/service/BookService.java
+++ BE/src/main/java/com/team7/chaekin/domain/book/service/BookService.java
@@ -1,12 +1,34 @@
 package com.team7.chaekin.domain.book.service;
 
+import com.team7.chaekin.domain.book.dto.BookListDto;
+import com.team7.chaekin.domain.book.dto.BookListResponse;
+import com.team7.chaekin.domain.book.dto.BookSearchRequest;
+import com.team7.chaekin.domain.book.entity.Book;
+import com.team7.chaekin.domain.book.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class BookService {
+    private final BookRepository bookRepository;
 
+    @Transactional(readOnly = true)
+    public BookListResponse search(BookSearchRequest bookSearchRequest, Pageable pageable){
+        Slice<Book> slice = bookRepository.findByTitleContaining(bookSearchRequest.getKeyword(), pageable);
+        List<BookListDto> list = new ArrayList<>();
+        for (Book book : slice) {
+            list.add(new BookListDto(book.getId(), book.getTitle(), book.getCover()));
+        }
+
+        return new BookListResponse(list);
+    }
 }
