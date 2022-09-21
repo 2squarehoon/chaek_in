370d5948d37950d4368007c138d0ee2c46a871e2
SH <shhh1006@daum.net>
2022-09-21 11:12:28 +0900

    💄 style : Change folder name lower case - 폴더명은 소문자로 - library component들, common/BookItem 생성

---
+++ FE/chaek-in/src/components/common/BookItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import styled from 'styled-components/native';
+
+function BookItem() {
+  return (
+    <>
+      <Text>개별 책</Text>
+    </>
+  );
+}
+
+export default BookItem;
