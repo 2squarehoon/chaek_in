3cc31023f8e2b8138297c005c1e6bc8a19e01289
김혜린 <herrine.kim@gmail.com>
2022-09-21 09:33:36 +0900

    🔨 chore : git reset 후 프론트엔드 코드 백업

--- FE/chaek-in/src/navigation/TabNavigation.js
+++ FE/chaek-in/src/navigation/TabNavigation.js
@@ -1,28 +1,25 @@
-import React from 'react'
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-// import HomeScreen from "../screens/HomeScreen";
-import RecommendScreen from '../screens/RecommendScreen'
-import MeetingScreen from '../screens/MeetingScreen'
-import RecordScreen from '../screens/RecordScreen'
-import HomeNavigation from './HomeNavigation'
-import ReadScreen from '../screens/ReadScreen'
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import RecomNavigation from './RecomNavigation';
+import MeetingScreen from '../screens/MeetingScreen';
+import HomeNavigation from './HomeNavigation';
+import ReadScreen from '../screens/ReadScreen';
+import RecordNavigation from './RecordNavigation';
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
-function TabNavigation () {
+function TabNavigation() {
   return (
     // <NavigationContainer>
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeNavigation} options={{ headerShown: false }} />
-      <Tab.Screen name="Recommend" component={RecommendScreen} />
-      <Tab.Screen name="Read" component={ReadScreen} />
-      <Tab.Screen name="Meeting" component={MeetingScreen} />
-      <Tab.Screen name="Record" component={RecordScreen} />
+      <Tab.Screen name='Home' component={HomeNavigation} options={{ headerShown: false }} />
+      <Tab.Screen name='Recommend' component={RecomNavigation} />
+      <Tab.Screen name='Read' component={ReadScreen} />
+      <Tab.Screen name='Meeting' component={MeetingScreen} />
+      <Tab.Screen name='Record' component={RecordNavigation} />
     </Tab.Navigator>
     // </NavigationContainer>
-  )
+  );
 }
 
-export default TabNavigation
+export default TabNavigation;
--- FE/chaek-in/src/screens/BookLogScreen.jsx
+++ FE/chaek-in/src/screens/BookLogScreen.jsx
@@ -1,19 +1,20 @@
 import {StyleSheet, Text, View} from "react-native";
+import React from "react";
 
-function BookLogScreen () {
+function BookLogScreen() {
   return (
     <View style={styles.container}>
       <Text>오늘의 책</Text>
     </View>
-  )
-};
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  }
-})
+  },
+});
 
 export default BookLogScreen;
