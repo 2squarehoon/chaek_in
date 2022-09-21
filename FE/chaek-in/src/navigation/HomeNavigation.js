3d391e1d8decaf05c4b672719c55e4d1190a6045
SH <shhh1006@daum.net>
2022-09-21 13:52:06 +0900

    🐛 fix : Rebuild login - 깃 꼬인거 초기화하면서 날아간 로그인 되살림

--- FE/chaek-in/src/navigation/HomeNavigation.js
+++ FE/chaek-in/src/navigation/HomeNavigation.js
@@ -2,6 +2,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import BookDetailScreen from '../screens/BookDetailScreen';
 import BookLogScreen from '../screens/BookLogScreen';
+import LoginScreen from '../screens/LoginScreen';
 
 const HomeStack = createStackNavigator();
 
@@ -11,6 +12,7 @@ function HomeNavigation() {
       <HomeStack.Screen name='Library' component={HomeScreen} options={{ title: '서재' }}></HomeStack.Screen>
       <HomeStack.Screen name='BookLogs' component={BookLogScreen}></HomeStack.Screen>
       <HomeStack.Screen name='BookDetail' component={BookDetailScreen}></HomeStack.Screen>
+      <HomeStack.Screen name='Login' component={LoginScreen}></HomeStack.Screen>
     </HomeStack.Navigator>
   );
 }
