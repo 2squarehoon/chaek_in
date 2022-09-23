e9c0f5c52abf9a2b045d8bede62dae914935000a
herrinekim <herrine.kim@gmail.com>
2022-09-23 15:11:17 +0900

    🐛 fix : RecomNavigation screens 경로

--- FE/chaek-in/src/navigation/RecomNavigation.js
+++ FE/chaek-in/src/navigation/RecomNavigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import OurRecomScreen from '../screens/recom/OurRecomScreen';
-import CFScreen from '../screens/recom/CFScreen';
+import OurRecomScreen from '../screens/recommendation/OurRecomScreen';
+import CFScreen from '../screens/recommendation/CFScreen';
 import CBFScreen from '../screens/recommendation/CBFScreen';
 
 const RecomTab = createMaterialTopTabNavigator();
