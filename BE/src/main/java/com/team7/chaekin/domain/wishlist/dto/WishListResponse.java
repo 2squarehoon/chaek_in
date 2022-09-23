5acc70a14773035769a5d9cd87d9bf6ce0d61917
김현교 <zkdlajt96@naver.com>
2022-09-23 16:33:55 +0900

    :sparkles: feat : 찜하기 기능 추가
    - 찜 목록 CRD 추가.

--- BE/src/main/java/com/team7/chaekin/domain/wishlist/dto/WishListResponse.java
+++ BE/src/main/java/com/team7/chaekin/domain/wishlist/dto/WishListResponse.java
@@ -1,9 +1,11 @@
 package com.team7.chaekin.domain.wishlist.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
+@AllArgsConstructor
 @Data
 public class WishListResponse {
     private List<WishListDto> wishlist;
