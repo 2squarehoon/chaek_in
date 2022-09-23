b357a9c78c2b360d6a47bd99e9f915e15405f7f1
김현교 <zkdlajt96@naver.com>
2022-09-23 15:31:08 +0900

    :sparkles: feat : 메모 기능 추가
    - 메모 CRUD 추가.

--- BE/src/main/java/com/team7/chaekin/domain/memo/dto/MemoListResponse.java
+++ BE/src/main/java/com/team7/chaekin/domain/memo/dto/MemoListResponse.java
@@ -1,10 +1,12 @@
 package com.team7.chaekin.domain.memo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
+@AllArgsConstructor
 @Data
 public class MemoListResponse {
     private List<MemoListDto> memos;
