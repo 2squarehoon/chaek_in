6e38501f4942017081b3b87a5d662273b6a9d328
SH <shhh1006@daum.net>
2022-09-24 00:26:43 +0900

    :sparkles: feat : Build auth flow - 앱 최상단에 SecureStore 토큰 저장유무로 메인기능 / 로그인 분기

--- FE/chaek-in/src/navigation/TabNavigation.js
+++ FE/chaek-in/src/navigation/TabNavigation.js
@@ -6,20 +6,18 @@ import HomeNavigation from './HomeNavigation';
 import ReadScreen from '../screens/ReadScreen';
 import RecordNavigation from './RecordNavigation';
 // import LoginScreen from '../screens/signin/LoginScreen';
-import SigninNavigation from './SigninNavigation';
 
 const Tab = createBottomTabNavigator();
 
 function TabNavigation() {
   return (
     // <NavigationContainer>
-    <Tab.Navigator>
-      <Tab.Screen name='Home' component={HomeNavigation} options={{ headerShown: false }} />
+    <Tab.Navigator initialRouteName='Library'>
+      <Tab.Screen name='Library' component={HomeNavigation} options={{ headerShown: false }} />
       <Tab.Screen name='Recommend' component={RecomNavigation} />
       <Tab.Screen name='Read' component={ReadScreen} />
       <Tab.Screen name='Meeting' component={MeetingScreen} />
       <Tab.Screen name='Record' component={RecordNavigation} />
-      <Tab.Screen name='Login' component={SigninNavigation} />
     </Tab.Navigator>
     // </NavigationContainer>
   );
