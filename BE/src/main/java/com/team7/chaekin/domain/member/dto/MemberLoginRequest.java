7969de8db0ce37a04eb5b347dc3ba8e66684231d
김현교 <zkdlajt96@naver.com>
2022-09-25 19:56:50 +0900

    :bug: fix : Enum에 NotBlank를 NotNull로 수정
    - Enum에 NotBlank를 NotNull로 수정.

--- BE/src/main/java/com/team7/chaekin/domain/member/dto/MemberLoginRequest.java
+++ BE/src/main/java/com/team7/chaekin/domain/member/dto/MemberLoginRequest.java
@@ -3,9 +3,11 @@ package com.team7.chaekin.domain.member.dto;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class MemberLoginRequest {
     @Email(message = "Must email format.")
+    @NotBlank
     private String identifier;
 }
