72cddce0d57652f417ac181ed51524e2c116c890
SH <shhh1006@daum.net>
2022-09-26 17:37:13 +0900

    :sparkles: feat : Build redux files - 리덕스 도입 시도중

--- FE/chaek-in/src/screens/signin/LoginScreen.jsx
+++ FE/chaek-in/src/screens/signin/LoginScreen.jsx
@@ -8,6 +8,8 @@ import styled from 'styled-components/native';
 import { HOST } from '@env';
 // import EncryptedStorage from 'react-native-encrypted-storage';
 import * as SecureStore from 'expo-secure-store';
+import { useSelector, useDispatch } from 'react-redux';
+import { setAccessToken, setEmail, setNickname, setRefreshToken } from '../../redux/actions';
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -15,9 +17,12 @@ function LoginScreen({ navigation }) {
   const [reqError, setReqError] = useState('');
   const [userEmail, setUserEmail] = useState();
   const [isFirst, setIsFirst] = useState('');
-  const [nname, setNickname] = useState('');
-  const [aToken, setAccessToken] = useState('');
-  const [rToken, setRefreshToken] = useState('');
+  const [nname, setNname] = useState('');
+  const [aToken, setAToken] = useState('');
+  const [rToken, setRToken] = useState('');
+
+  const { nickname, email, accessToken, refreshToken } = useSelector((state) => state.userReducer);
+  const dispatch = useDispatch();
 
   const [request, response, promptAsync] = Google.useAuthRequest({
     expoClientId: GOOGLE_EXPO_CLIENT_ID,
@@ -55,12 +60,21 @@ function LoginScreen({ navigation }) {
     save('refreshToken', rToken);
     await console.log('SecureStore 저장됨');
   };
+
+  // redux state에 저장
+  const saveReduxState = () => {
+    dispatch(setNickname(nname));
+    dispatch(setEmail(userEmail));
+    dispatch(setRefreshToken(rToken));
+    dispatch(setAccessToken(aToken));
+  };
   // isFirst 값이 갱신되면 실행, 처음 로그인이면 추가정보입력으로 이동, 아닐 시 SecureStore에 토큰, 정보들 저장
   useEffect(() => {
     if (isFirst) {
       navigation.navigate('Nickname', { email: userEmail });
     } else if (isFirst === false) {
       saveStore();
+      saveReduxState();
     }
   }, [isFirst]);
 
@@ -91,9 +105,9 @@ function LoginScreen({ navigation }) {
     await Axios.get(`${HOST}/api/v1/members/login?identifier=${mail}`)
       .then(function (response) {
         console.log(response.data);
-        setNickname(response.data.nickname);
-        setAccessToken(response.data.accessToken);
-        setRefreshToken(response.data.refreshToken);
+        setNname(response.data.nickname);
+        setAToken(response.data.accessToken);
+        setRToken(response.data.refreshToken);
         setIsFirst(response.data.isFirst);
       })
       .catch(function (error) {
