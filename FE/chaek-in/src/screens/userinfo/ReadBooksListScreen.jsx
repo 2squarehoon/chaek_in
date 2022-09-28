881a1fc131b3fa384dea672388925330ade6c7a8
SH <shhh1006@daum.net>
2022-09-28 15:54:05 +0900

    :sparkles: feat : Build my pages without any design - 페이지 내비게이션과 api 연동되는 것만 구현 - 찜하기 기능이 구현되지 않아 wishlist도 아직 못함
    Former-commit-id: 0d0f54ce37e0de8970aa511cd658966a1197a284

--- FE/chaek-in/src/screens/userinfo/ReadBooksListScreen.jsx
+++ FE/chaek-in/src/screens/userinfo/ReadBooksListScreen.jsx
@@ -1,11 +1,42 @@
-// import styled from 'styled-components/native';
-import React from 'react';
-import { View, Text } from 'react-native';
+import styled from 'styled-components/native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import Axios from 'axios';
+import { HOST } from '@env';
+import { useSelector } from 'react-redux';
+import BookItem from '../../components/common/BookItem';
 
-function ReadBooksListScreen() {
+function ReadBooksListScreen({ navigation }) {
+  const { accessToken } = useSelector((state) => state.main);
+  const [readBooks, setreadBooks] = useState([]);
+
+  useEffect(() => {
+    Axios.get(`${HOST}/api/v1/books/me?isReading=true`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then(function (response) {
+        console.log(response.data);
+        setreadBooks(response.data.books);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  const goBookDetail = (bookNumber) => {
+    navigation.navigate('BookDetail', { bookId: bookNumber });
+  };
+
   return (
     <View>
       <Text>읽은 책 리스트 페이지</Text>
+      {readBooks.map((book) => (
+        <TouchableOpacity key={book.bookId} onPress={() => goBookDetail(book.bookId)}>
+          <BookItem item={book} />
+        </TouchableOpacity>
+      ))}
     </View>
   );
 }
