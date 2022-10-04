0bb8b951ee11ffe6a70980887af0519aa27ea704
김현교 <zkdlajt96@naver.com>
2022-10-04 14:35:38 +0900

    :recycle: refactor : calender api 응답 수정
    - 달력 라이브러리에 맞게 api 응답 수정.
    
    
    Former-commit-id: baf01393a5ab79403478a10d32783985d0fe95b1

--- BE/src/main/java/com/team7/chaekin/domain/book/dto/BookCalenderDto.java
+++ BE/src/main/java/com/team7/chaekin/domain/book/dto/BookCalenderDto.java
@@ -8,4 +8,6 @@ import lombok.Data;
 public class BookCalenderDto {
     private long bookId;
     private String title;
+    private Boolean isStartDay;
+    private Boolean isEndDay;
 }
--- BE/src/main/java/com/team7/chaekin/domain/book/dto/BookCalenderListDto.java
+++ BE/src/main/java/com/team7/chaekin/domain/book/dto/BookCalenderListDto.java
@@ -9,6 +9,5 @@ import java.util.List;
 @Data
 public class BookCalenderListDto {
     private int day;
-    private Boolean isExist;
     private List<BookCalenderDto> books;
 }
